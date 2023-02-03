b5fec4e3507b61328a7b98813a949d7c6aa47ef4
chadji15 <chadji15@ucy.ac.cy>
2023-02-03 15:29:12 +0100

    add optimal parameters

--- src/main.py
+++ src/main.py
@@ -12,6 +12,36 @@ random.seed(0)
 np.random.seed(0)
 torch.manual_seed(0)
 
+optimal_parameters = {'100': {'batch_size': 4, 'learning_rate': 0.001, 'weighted_sampling': True},
+                      '102': {'batch_size': 4, 'learning_rate': 0.001, 'weighted_sampling': True},
+                      '104': {'batch_size': 4, 'learning_rate': 0.001, 'weighted_sampling': False},
+                      '105': {'batch_size': 4, 'learning_rate': 0.001, 'weighted_sampling': False},
+                      '106': {'batch_size': 16, 'learning_rate': 0.001, 'weighted_sampling': True},
+                      '108': {'batch_size': 32, 'learning_rate': 0.01, 'weighted_sampling': True},
+                      '114': {'batch_size': 4, 'learning_rate': 0.001, 'weighted_sampling': False},
+                      '116': {'batch_size': 16, 'learning_rate': 0.001, 'weighted_sampling': True},
+                      '119': {'batch_size': 4, 'learning_rate': 0.01, 'weighted_sampling': True},
+                      '200': {'batch_size': 32, 'learning_rate': 0.01, 'weighted_sampling': True},
+                      '202': {'batch_size': 16, 'learning_rate': 0.001, 'weighted_sampling': True},
+                      '201': {'batch_size': 32, 'learning_rate': 0.001, 'weighted_sampling': True},
+                      '203': {'batch_size': 4, 'learning_rate': 0.001, 'weighted_sampling': True},
+                      '205': {'batch_size': 32, 'learning_rate': 0.001, 'weighted_sampling': True},
+                      '208': {'batch_size': 4, 'learning_rate': 0.01, 'weighted_sampling': False},
+                      '209': {'batch_size': 4, 'learning_rate': 0.001, 'weighted_sampling': False},
+                      '212': {'batch_size': 16, 'learning_rate': 0.001, 'weighted_sampling': True},
+                      '210': {'batch_size': 16, 'learning_rate': 0.001, 'weighted_sampling': True},
+                      '213': {'batch_size': 4, 'learning_rate': 0.001, 'weighted_sampling': True},
+                      '215': {'batch_size': 4, 'learning_rate': 0.001, 'weighted_sampling': True},
+                      '217': {'batch_size': 4, 'learning_rate': 0.001, 'weighted_sampling': True},
+                      '219': {'batch_size': 16, 'learning_rate': 0.01, 'weighted_sampling': True},
+                      '220': {'batch_size': 16, 'learning_rate': 0.01, 'weighted_sampling': True},
+                      '222': {'batch_size': 32, 'learning_rate': 0.001, 'weighted_sampling': False},
+                      '221': {'batch_size': 32, 'learning_rate': 0.001, 'weighted_sampling': False},
+                      '228': {'batch_size': 16, 'learning_rate': 0.001, 'weighted_sampling': True},
+                      '223': {'batch_size': 16, 'learning_rate': 0.001, 'weighted_sampling': True},
+                      '231': {'batch_size': 4, 'learning_rate': 0.001, 'weighted_sampling': True},
+                      '233': {'batch_size': 32, 'learning_rate': 0.001, 'weighted_sampling': False}}
+
 
 def main():
     opt = parse_opts()
@@ -34,6 +64,11 @@ def main():
         opt.early_stopping = True
         for patient in opt.selected_patients_fine_tuning:
             print('\nPatient:', patient)
+            #MAKE SURE TO INFORM THAT THESE PARAMETERS ARE OVERRIDEN
+            if patient in optimal_parameters.keys():
+                opt.batch_size = optimal_parameters[patient]['batch_size']
+                opt.learning_rate = optimal_parameters[patient]['learning_rate']
+                opt.weighted_sampling = optimal_parameters[patient]['weighted_sampling']
             test_bal_acc = run(opt, [patient])
             test_balanced_accuracy.append(test_bal_acc)
 
