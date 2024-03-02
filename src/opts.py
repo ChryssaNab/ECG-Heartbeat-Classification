b12b7adc47d673b0aa6429c316d82a7372d9383a
ChryssaNab <chryssanabdmst@gmail.com>
2024-03-02 03:58:17 +0200

    Update arguments usage

--- src/opts.py
+++ src/opts.py
@@ -9,17 +9,17 @@ def parse_opts():
         '--data_path',
         default='./dataset/mitbih_database/',
         type=str,
-        help='Data directory path')
+        help='The data directory path under which the dataset lies.')
     parser.add_argument(
         '--output_path',
         default='./output/',
         type=str,
-        help='Output directory path')
+        help='The output directory path where the checkpoints and log files are created.')
     parser.add_argument(
         '--state',
         default='pre-training',
         type=str,
-        help='(pre-training | individuals | fine_tuning)')
+        help='(pre-training | baseline individuals | fine-tuning individuals)')
     parser.add_argument(
         '--selected_patients_fine_tuning',
         default=['100', '102', '104', '105', '106', '108', '114', '116', '119', '200', '202', '201', '203', '205',
@@ -27,14 +27,16 @@ def parse_opts():
                  '233'],
         type=str,
         nargs='+',
-        help='The selected patients for the individuals and fine-tuning modes')
+        help='The list of the selected patients earmarked for the experiments. '
+             'Only applies to the baseline individual models and fine-tuning models that target certain patients.')
     parser.add_argument(
         '--input_size',
         default=128,
         type=int,
         help='Input dimension')
     parser.add_argument(
-        '--pretrain_path', default='./output/save_16.pth', type=str, help='Pretrained model (.pth)')
+        '--pretrain_path', default='./output/save_16.pth', type=str,
+        help='The pre-trained model checkpoint (.pth)')
 
     # |------------------------------------------ CNN default settings ----------------------------------------------|
     parser.add_argument(
@@ -69,7 +71,7 @@ def parse_opts():
     parser.add_argument(
         '--early_stopping',
         action='store_false',
-        help='If true, we are on individuals or fine-tuning mode.')
+        help='If true, we are on baseline or fine-tuning mode.')
     parser.set_defaults(early_stopping=False)
     parser.add_argument(
         '--n_epochs',
